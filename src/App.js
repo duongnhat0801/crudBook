107e15a6faf4874dff8b3f26fd81f75a5258bbc3
duongnhat0801 <nhatx0801@hmail.com>
2024-08-26 07:00:21 +0700

    bookCRUD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BookCreate from "./component/BookCreate";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import 'react-toastify/dist/ReactToastify.css';
+import BookList from "./component/BookList";
+import BookEdit from "./component/BookEdit";
+import {ToastContainer} from "react-toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/create" element={<BookCreate />} />
+            <Route path="/" element={<BookList />} />
+            <Route path="/editBook/:id" element={<BookEdit />} />
+          </Routes>
+        </BrowserRouter>
+        <ToastContainer />
+      </>
   );
 }
 
